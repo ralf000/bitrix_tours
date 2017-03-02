afa5b1a276fbbdc65501aa2cc177522ee38e9d9a
ralf <ralf000@yandex.ru>
2017-03-02 16:40:21 +0300

    Начало создания нового шаблона (Список шаблонов - добавить шаблон)

---
+++ bitrix/templates/tour_index/footer.php
@@ -0,0 +1,230 @@
+
+						<div class="row">
+							<div class="col-sm-6">
+
+								<div class="green-box">
+									<div class="title">
+										<h2>
+											<a href="#">Новости</a>
+										</h2>
+									</div>
+									<div class="cnt">
+
+										<div class="item">
+											<div class="preview">
+												<img src="img/news/доминикана.png">
+											</div>
+											<div class="n-staff">
+												<span class="date">30.10.2014</span>
+												<h3>
+													<a href="#">Отдых в Доминикане</a>
+												</h3>
+												<p class="txt">
+													Мы открываем новую возможность для наших клиентов - отдых в Доминикане. 
+												</p>
+												<div class="more-btn-box">
+													<a href="#" class="link">Подробнее</a>
+												</div>
+											</div>
+										</div>
+
+										<div class="item">
+											<div class="preview">
+												<img src="img/news/лучшее туристическое агентсво.png">
+											</div>
+											<div class="n-staff">
+												<span class="date">23.10.2014</span>
+												<h3>
+													<a href="#">2-е место</a>
+												</h3>
+												<p class="txt">
+													22 октября наша компания заняла 2-е место в номинации "Лучшее туристическое агентство 2014". 
+												</p>
+												<div class="more-btn-box">
+													<a href="#" class="link">Подробнее</a>
+												</div>
+											</div>
+										</div>
+
+										<div class="item">
+											<div class="preview">
+												<img src="img/news/таиланд.png">
+											</div>
+											<div class="n-staff">
+												<span class="date">20.10.2014</span>
+												<h3>
+													<a href="#">Экстрим в Таиланде</a>
+												</h3>
+												<p class="txt">
+													Наш коллега Дмитрий Коновалов недавно вернулся из Таиланда и хочет поделиться своими впечатлениями об экстримальном отдыхе в этой стране.
+												</p>
+												<div class="more-btn-box">
+													<a href="#" class="link">Подробнее</a>
+												</div>
+											</div>
+										</div>
+										
+									</div>
+								</div>
+
+							</div>
+							<div class="col-sm-6">
+
+								<div class="green-box">
+									<div class="title">
+										<h2>
+											<a href="#">Акции</a>
+										</h2>
+									</div>
+									<div class="cnt">
+
+										<div class="item">
+											<div class="preview">
+												<img src="img/news/греция.png">
+											</div>
+											<div class="n-staff">
+												<span class="date">23.10.2014</span>
+												<h3>
+													<a href="#">Путёвка в Грецию на двоих всего за 50000!</a>
+												</h3>
+												<p class="txt">
+													Не пропустите новое специальное предложение!
+												</p>
+												<div class="more-btn-box">
+													<a href="#" class="link">Подробнее</a>
+												</div>
+											</div>
+										</div>
+
+										<div class="item">
+											<div class="preview">
+												<img src="img/news/подарок на день рождения.png">
+											</div>
+											<div class="n-staff">
+												<span class="date">1.10.2014</span>
+												<h3>
+													<a href="#">Подарок в честь Дня Рождения!</a>
+												</h3>
+												<p class="txt">
+													В День Рождения кадому клиенту дарим скидку 15%!!!
+												</p>
+												<div class="more-btn-box">
+													<a href="#" class="link">Подробнее</a>
+												</div>
+											</div>
+										</div>
+
+										<div class="item">
+											<div class="preview">
+												<img src="img/news/лыжи.png">
+											</div>
+											<div class="n-staff">
+												<span class="date">10.09.2014</span>
+												<h3>
+													<a href="#">Скидки на горнолыжные курорты!</a>
+												</h3>
+												<p class="txt">
+													Начинается горнолыжный сезон! Для Вас - специальное предложение!
+												</p>
+												<div class="more-btn-box">
+													<a href="#" class="link">Подробнее</a>
+												</div>
+											</div>
+										</div>
+										
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+
+						<div class="sidebar">
+							<div class="side-tit">
+								<a href="#">Горящие туры</a>
+							</div>
+							<ul class="items">
+								<li><a href="#">Турция, Анталия, ADELA HOTEL 4 *, всё включено, 12 ноября</a></li>
+								<li><a href="#">Турция, Кемер, AKKA CLAROS HOTEL 4 *, всё включено, 10 ноября</a></li>
+								<li><a href="#">Кипр, Айя-Напа, AMALTHIA VILLAS Villa, только завтрак, 1 ноября</a></li>
+								<li><a href="#">Кипр, Айя-Напа, AMALTHIA VILLAS Villa, только завтрак, 7 ноября</a></li>
+								<li><a href="#">Таиланд, Паттайя, A-ONE PATTAYA BEACH RESORT 4 *, всё включено, 15 ноября</a></li>
+								<li><a href="#">Таиланд, Пхукет, ALEENTA RESORT & SPA PHUKET 5 *, всё включено, 10 ноября</a></li>
+								<li><a href="#">Таиланд, Пхукет, AMARI PHUKET 4 *, всё включено, 9 ноября</a></li>
+								<li><a href="#">Таиланд, Пхукет, AMARI WATERGATE 5 *, всё включено, 9 ноября</a></li>
+								<li><a href="#">Таиланд, Самуи+Бангкок, BAAN HAAD NGAM 4 *, завтрак + обед, 1 ноября</a></li>
+							</ul>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<!-- counter -->
+			<div class="counter">
+					<div class="container">								
+						<span class="sign">Наши клиенты:</span>
+						<span class="num">6 523</span>
+					</div>
+			</div>
+			<!-- /counter -->			
+		</div>
+		<!-- /content -->
+		
+		<!-- footer -->
+		<footer class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-7 col-lg-8">
+						<ul class="footer-menu">
+							<li>
+								<ul>
+									<li><a href="#">об агентстве</a></li>
+									<li><a href="#">новости</a></li>
+									<li><a href="#">акции</a></li>
+									<li><a href="#">подбор тура</a></li>
+									<li><a href="#">контакты</a></li>
+								</ul>
+							</li>
+							<li>
+								<ul>
+									<li><a href="#">поиск</a></li>
+									<li><a href="#">карта сайта</a></li>
+									<li><a href="#">авторизация</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<div class="col-sm-5 col-lg-4">
+						<div class="search-box">
+							<form action="#">
+								<input type="text" placeholder="Поиск...">
+								<div class="s-buttom">
+									<input type="submit" value="">
+								</div>
+							</form>
+						</div>
+						<ul class="social-links">
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="copy">
+					<div class="left">© Туристическое агентство, 2014</div>
+					<div class="right">
+						<a href="#">Разработка сайта - Buroff</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- /footer -->
+
+		<!-- Javascripts -->
+		<script src="js/jquery-2.1.0.min.js"></script>
+		<script src="js/jquery.bxslider.min.js"></script>
+		<script src="js/main.js"></script>
+		<!-- / Javascripts -->
+	</body>
+</html>
